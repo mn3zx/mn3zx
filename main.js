a4c78dc26cc419d2f47a43fde69e69a89010ac97
mn3zx <113049734+mn3zx@users.noreply.github.com>
2022-10-17 16:13:40 -0300

    v1.3
    -Added Clock and Date
    -Minor aesthetic changes

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ function giraBg() {
   }
 }
 setInterval(giraBg,5);
+
+function clock() {
+  let tempo = new Date();
+  let ano = tempo.getFullYear();
+  let mes = tempo.getMonth()+1;
+  let diaMes = tempo.getDate();
+  let diaSemana = tempo.getDay();
+  let hora = tempo.getHours();
+  let minuto = tempo.getMinutes();
+  let segundo = tempo.getSeconds();
+  let relogio = hora + ":" + minuto + ":" + segundo;
+  let calendario = diaMes + "/" + mes + "/" + ano;
+  let display = window.document.getElementById('clock');
+  display.innerHTML = calendario + "<br>" + relogio;
+}
+setInterval(clock,500)
